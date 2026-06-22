33956f717357a2c9824a43f32acdd99669c8a0ee
GUOwendong <1965905496@qq.com>
2026-06-22 14:46:22 +0800

    add test product case flow

--- tests/test_octopus/test_product_flow.py
+++ tests/test_octopus/test_product_flow.py
@@ -4,6 +4,9 @@
 @Author: guowendong
 @Desc: Conducting code practice and testing development work
 """
+import io
+
+import pandas as pd
 import requests
 
 from common.log_utils import log
@@ -12,6 +15,7 @@ BASE_URL = "http://api.wxorder.taover.com/v1"
 HEADERS = {
     "Authorization": "Bearer==eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhZG1pbiIsImF1ZCI6IjA5OGY2YmNkNDYyMWQzNzNjYWRlNGU4MzI2MjdiNGY2IiwidGVuYW50IjoiMjM3IiwidXNlcm5hbWUiOiIxNzYxMTIxOTgwMyIsInVzZXJpZCI6IjMzMiIsInBhc3N3b3JkIjoiYmE2NzY2NThlOWMxN2M3ZmU4ZjcxNTliNjZiZTVkZDAiLCJzdGF0dXMiOjEsImV4cCI6MTc4MjMyMDM5OSwibmJmIjoxNzgyMDk1ODk4fQ.iFt3Sr5uf5N7DI9ATDJetWddl0iKKUgDOSqTj5ddb8I"
 }
+ID = None
 SKU_ID = None
 GOODS_NAME = "西瓜"
 
@@ -57,9 +61,10 @@ class TestProductManager:
         log.info(f"✅ 商品创建成功，🆔：{data['data']['firstSkuId']}，Name：{GOODS_NAME}")
 
     def test_query_product(self):
+        global ID
         log.info("🔍开始搜索商品...")
         response = requests.get(
-            url=f"{BASE_URL}/goodsbase", headers=HEADERS, params={"goodsName": {GOODS_NAME}, "saleStatus": "1"}
+            url=f"{BASE_URL}/goodsbase", headers=HEADERS, params={"goodsName": GOODS_NAME, "saleStatus": "1"}
         )
 
         data = response.json()
@@ -73,7 +78,8 @@ class TestProductManager:
             data["data"]["rows"][0]["goodsName"] == GOODS_NAME
         ), f"查询结果与预期不同，实际商品名称：{data['data']['rows'][0]['goodsName']}"
 
-        log.info(f"🔍搜索出来的是{data['data']['rows'][0]['goodsName']}")
+        ID = data["data"]["rows"][0]["id"]
+        log.info(f"🔍搜索出来的是{GOODS_NAME}，🆔：{ID}")
 
     def test_delisting_product(self):
         log.info("⬇️开始下架商品...")
@@ -113,17 +119,20 @@ class TestProductManager:
 
         log.info("⬆️商品上架🏅")
 
-        # def test_delete_product(self):
-        #     log.info("🗑️开始删除商品...")
-        #     response = requests.get(
-        #         url=f"{BASE_URL}/goodsbase/deleteGoods",
-        #         headers=HEADERS,
-        #         params={"goodsId": SKU_ID}
-        #     )
-        #
-        #     data = response.json()
+    def test_delete_product(self):
+        log.info("🗑️开始删除商品...")
+        response = requests.get(url=f"{BASE_URL}/goodsbase/deleteGoods", headers=HEADERS, params={"idList": ID})
+
+        df = pd.read_excel(io.BytesIO(response.content))
+        data = df.to_dict(orient="records")
+        print(data)
 
         # HTTP层断言
         assert response.status_code == 200, f"HTTP状态码异常: {response.status_code}"
 
         # 业务层断言
+        assert len(data) > 0, "删除结果 Excel 为空！"
+        assert data[0]["处理结果"] == "删除成功"
+        assert int(data[0]["商品ID"]) == int(ID)
+
+        log.info("🗑️商品删除成功🏅")
